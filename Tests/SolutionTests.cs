965db247609a73e7c57e138abc8f6b148130b610
Libberator <madlibberator@gmail.com>
2025-01-07 18:57:12 -0800

    Squashed commit from upstream

--- Tests/SolutionTests.cs
+++ Tests/SolutionTests.cs
@@ -16,7 +16,7 @@ namespace Aoc.Tests;
 public static class SolutionTests
 {
     /* Adjust year(s) here */
-    private const int StartYear = 2024;
+    private const int StartYear = 2015;
 
     private const int StopYear = 2024;
 
@@ -38,22 +38,19 @@ public static class SolutionTests
 
         // Act
         solver.Setup(input);
-        if (!string.IsNullOrEmpty(expected1))
-        {
-            var part1Result = solver.SolvePart1()?.ToString();
-            assertions.Add(() =>
-                Assert.That(part1Result, Is.EqualTo(expected1), "Part 1 does not match expected results."));
-        }
-
-        if (!string.IsNullOrEmpty(expected2))
-        {
-            var part2Result = solver.SolvePart2()?.ToString();
-            assertions.Add(() =>
-                Assert.That(part2Result, Is.EqualTo(expected2), "Part 2 does not match expected results."));
-        }
+        var part1Result = solver.SolvePart1()?.ToString();
+        var part2Result = solver.SolvePart2()?.ToString();
 
         // Assert
-        Assert.Multiple(() => assertions.ForEach(assertion => assertion()));
+        if (!string.IsNullOrEmpty(expected1))
+            assertions.Add(() =>
+                Assert.That(part1Result, Is.EqualTo(expected1), "Part 1 does not match expected results."));
+
+        if (!string.IsNullOrEmpty(expected2))
+            assertions.Add(() =>
+                Assert.That(part2Result, Is.EqualTo(expected2), "Part 2 does not match expected results."));
+
+        Assert.Multiple(() => assertions.ForEach(assertion => assertion.Invoke()));
     }
 
     private static IEnumerable<TestCaseData> GenerateTestsForYears() => Years.SelectMany(GenerateTestsForYear);
