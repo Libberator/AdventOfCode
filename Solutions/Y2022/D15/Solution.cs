b989e2f204a44099ebddf3c963c59ef6ea7d391c
Libberator <madlibberator@gmail.com>
2025-01-14 00:26:23 -0800

    Y22,D15-16

--- Solutions/Y2022/D15/Solution.cs
+++ Solutions/Y2022/D15/Solution.cs
@@ -1,19 +1,104 @@
+using System;
+using System.Collections.Generic;
+using AoC.Utilities.Extensions;
+using AoC.Utilities.Geometry;
+
 namespace AoC.Solutions.Y2022.D15;
 
 public class Solution : ISolver
 {
+    private readonly List<Data> _data = [];
+    [TestValue(10)] private readonly int _row = 2_000_000;
+
     public void Setup(string[] input)
     {
-        // process input
+        foreach (var line in input)
+        {
+            var matches = Utils.NumberPattern().Matches(line);
+            var sensor = new Vec2D(int.Parse(matches[0].ValueSpan), int.Parse(matches[1].ValueSpan));
+            var beacon = new Vec2D(int.Parse(matches[2].ValueSpan), int.Parse(matches[3].ValueSpan));
+            _data.Add(new Data(sensor, beacon, sensor.DistanceManhattan(beacon)));
+        }
     }
 
     public object SolvePart1()
     {
-        return "Part 1";
+        HashSet<int> beaconsAlongRow = [];
+        SortedList<int, int> minMaxRanges = [];
+
+        foreach (var data in _data)
+        {
+            if (data.Beacon.Y == _row)
+                beaconsAlongRow.Add(data.Beacon.X);
+
+            var deltaY = Math.Abs(_row - data.Sensor.Y);
+            if (data.Distance < deltaY) continue; // doesn't touch the ROW
+
+            var minX = data.Sensor.X - (data.Distance - deltaY);
+            var maxX = data.Sensor.X + (data.Distance - deltaY);
+
+            if (!minMaxRanges.TryAdd(minX, maxX))
+                minMaxRanges[minX] = Math.Max(minMaxRanges[minX], maxX);
+        }
+
+        var occupiedCount = 0;
+        var xMin = int.MinValue;
+        foreach (var (min, max) in minMaxRanges)
+        {
+            xMin = Math.Max(xMin, min);
+            if (xMin > max) continue;
+
+            occupiedCount += max - xMin + 1;
+            xMin = max + 1;
+        }
+
+        return occupiedCount - beaconsAlongRow.Count;
     }
 
     public object SolvePart2()
     {
-        return "Part 2";
+        var beacon = FindBeacon();
+        var tuningFrequency = beacon.X * 4_000_000L + beacon.Y;
+        return tuningFrequency;
     }
+
+    // This takes advantage of the fact that the hidden beacon must be just outside the edge of the sensor's range
+    // Specifically, there will be 2 pairs of sensors to cover each side of the hidden beacon
+    // Using the lines along the gaps between the pairs (they will be perpendicular), X marks the spot
+    private Vec2D FindBeacon()
+    {
+        List<(Vec2D Point, Vec2D Dir)> lines = [];
+
+        for (var i = 0; i < _data.Count - 1; i++)
+        {
+            var a = _data[i];
+            for (var j = i + 1; j < _data.Count; j++)
+            {
+                var b = _data[j];
+                var distance = a.Sensor.DistanceManhattan(b.Sensor);
+                if (a.Distance + b.Distance + 2 != distance) continue;
+
+                var slope = a.Sensor.X < b.Sensor.X == a.Sensor.Y < b.Sensor.Y
+                    ? Vec2D.NE
+                    : Vec2D.SE;
+                if (lines.Count == 1 && lines[0].Dir == slope) // can't make an X with 2 parallel lines
+                    continue;
+
+                var point = a.Sensor.X < b.Sensor.X
+                    ? a.Sensor + (a.Distance + 1) * Vec2D.S
+                    : b.Sensor + (b.Distance + 1) * Vec2D.S;
+                lines.Add((point, slope));
+                if (lines.Count == 2) break;
+            }
+
+            if (lines.Count == 2) break;
+        }
+
+        var (pt1, dir1) = lines[0];
+        var (pt2, dir2) = lines[1];
+        Vec2D.LineIntersect(pt1, dir1, pt2, dir2, out var intersect);
+        return intersect;
+    }
+
+    private record Data(Vec2D Sensor, Vec2D Beacon, int Distance);
 }
--- Solutions/Y2022/D16/Solution.cs
+++ Solutions/Y2022/D16/Solution.cs
@@ -1,19 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AoC.Utilities.Graphs;
+
 namespace AoC.Solutions.Y2022.D16;
 
 public class Solution : ISolver
 {
+    private const string Start = "AA";
+    private readonly List<Valve> _valves = [];
+    private Dictionary<(string From, string To), int> _distanceCosts = [];
+
     public void Setup(string[] input)
     {
-        // process input
+        const string pattern = @"Valve (.{2}) has flow rate=(\d+); tunnel(?:s)? lead(?:s)? to valve(?:s)? (.+)";
+        Dictionary<string, HashSet<string>> adjacencyList = new();
+
+        foreach (var line in input)
+        {
+            var match = Regex.Match(line, pattern);
+            var valveId = match.Groups[1].Value;
+            var flowRate = int.Parse(match.Groups[2].ValueSpan);
+            var neighbors = match.Groups[3].Value.Split(", ");
+            adjacencyList.Add(valveId, [..neighbors]);
+            if (flowRate > 0)
+                _valves.Add(new Valve(valveId, 1 << _valves.Count, flowRate));
+        }
+
+        _distanceCosts = Graph.FindShortestPathCosts(adjacencyList);
     }
 
-    public object SolvePart1()
+    public object SolvePart1() => FindHighestPressure(30);
+
+    public object SolvePart2() => FindHighestDualPressure(26);
+
+    private int FindHighestPressure(int minutes, string start = Start)
     {
-        return "Part 1";
+        var maxPressure = int.MinValue;
+        Dictionary<int, int> snapshots = new();
+        Stack<(string Id, int Opened, int TimeRemaining, int Pressure)> stack = [];
+        stack.Push((start, 0, minutes, 0));
+
+        while (stack.Count > 0)
+        {
+            var (current, opened, timeLeft, pressure) = stack.Pop();
+            if (pressure > maxPressure) maxPressure = pressure;
+
+            // prune
+            if (MaxTheoreticalPressure(current, opened, timeLeft, pressure) <= maxPressure) continue;
+            if (snapshots.TryGetValue(opened, out var prevBest) && pressure <= prevBest) continue;
+            snapshots[opened] = pressure;
+
+            // go to next nodes
+            foreach (var n in NextNodes(current, opened, timeLeft))
+                stack.Push((n.Id, opened | n.IdMask, n.Remaining, pressure + n.Relieved));
+        }
+
+        return maxPressure;
     }
 
-    public object SolvePart2()
+    private IEnumerable<(string Id, int IdMask, int Remaining, int Relieved)> NextNodes(string node, int opened,
+        int minutes)
     {
-        return "Part 2";
+        foreach (var (id, idMask, flow) in _valves)
+        {
+            if ((opened & idMask) != 0) continue; // opened valve already
+            var timeRemaining = minutes - _distanceCosts[(node, id)] - 1;
+            if (timeRemaining <= 0) continue; // can't benefit from going there 
+            var pressureRelieved = flow * timeRemaining;
+            yield return (id, idMask, timeRemaining, pressureRelieved);
+        }
     }
+
+    private int MaxTheoreticalPressure(string node, int opened, int minutes, int pressure) =>
+        pressure + NextNodes(node, opened, minutes).Sum(next => next.Relieved);
+
+    private int FindHighestDualPressure(int minutes, string start = Start)
+    {
+        var maxPressure = int.MinValue;
+        Dictionary<int, int> snapshots = new();
+        Stack<(string ANode, string BNode, int Opened, int ARemaining, int BRemaining, int Pressure)> stack = new();
+        stack.Push((start, start, 0, minutes, minutes, 0));
+
+        while (stack.Count > 0)
+        {
+            var (a, b, opened, aTime, bTime, pressure) = stack.Pop();
+            if (pressure > maxPressure) maxPressure = pressure;
+
+            // prune
+            if (MaxTheoreticalDualPressure(a, b, opened, aTime, bTime, pressure) <= maxPressure) continue;
+            if (snapshots.TryGetValue(opened, out var prevBest) && pressure <= prevBest) continue;
+            snapshots[opened] = pressure;
+
+            // Prepare next nodes
+            var aNeighbors = NextNodes(a, opened, aTime).ToList();
+            var bNeighbors = NextNodes(b, opened, bTime).ToList();
+            switch (aNeighbors.Count, bNeighbors.Count)
+            {
+                case (0, 0):
+                    continue;
+                case (0, _):
+                    aNeighbors.Add((a, 0, 0, 0)); // stay still while b continues
+                    break;
+                case (_, 0):
+                    bNeighbors.Add((b, 0, 0, 0)); // stay still while a continues
+                    break;
+            }
+
+            // go to valid combos of next nodes
+            foreach (var (aNode, aMask, aRemaining, aRelieved) in aNeighbors)
+                foreach (var (bNode, bMask, bRemaining, bRelieved) in bNeighbors)
+                {
+                    if (aMask == bMask) continue;
+                    stack.Push((aNode, bNode, opened | aMask | bMask, aRemaining, bRemaining,
+                        pressure + aRelieved + bRelieved));
+                }
+        }
+
+        return maxPressure;
+    }
+
+    private int MaxTheoreticalDualPressure(string a, string b, int opened, int aTime, int bTime, int pressure)
+    {
+        var excess = 0;
+        foreach (var (id, idMask, flow) in _valves)
+        {
+            if ((opened & idMask) != 0) continue; // opened valve already
+            var time = Math.Max(aTime - _distanceCosts[(a, id)] - 1, bTime - _distanceCosts[(b, id)] - 1);
+            excess += flow * time;
+        }
+
+        return pressure + excess;
+    }
+
+    private record Valve(string Id, int IdMask, int Flow);
 }
